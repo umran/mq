488289cf9ac40327114472de60e3827cc866d669
Umran Hussein <umran.hussein@gmail.com>
2020-03-27 05:11:36 -0700

    improved documentation

--- config.go
+++ config.go
@@ -1,11 +1,17 @@
 package mq
 
-// Config represents configuration information for the cloud environment.
+// Config represents configuration information for the cloud provider.
 // The Provider can currently be either "aws" or "gcloud".
 // If the Provider is "aws", then AWSRegion must be set.
 // If the Provider is "gcloud", then GCloudProject must be set.
 type Config struct {
-	Provider      string
-	AWSRegion     string
+	// Provider represents the cloud provider.
+	// Currently this can either be "aws" or "gcloud"
+	Provider string
+
+	// AWSRegion represents which AWS region to connect to
+	AWSRegion string
+
+	// GCloudProject represents which GCloud project to connect to
 	GCloudProject string
 }
