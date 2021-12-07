7fbff6d1c55113d98b1ecb8158b1c58e6221f2af
Umran Hussain <umran.hussein@gmail.com>
2021-12-07 11:55:21 -0800

    made a revision

--- subscription_options.go
+++ subscription_options.go
@@ -7,23 +7,24 @@ type SubscriptionOptions struct {
 
 	// EnableMessageOrdering is a boolean for whether messages with the same
 	// ordering key should be ordered according to when they were published.
-	// Creates a FIFO queue if using AWS SQS
-	// Defaults to false
+	// Creates a FIFO queue if using AWS SQS.
+	// Defaults to false.
 	EnableMessageOrdering bool
 
 	// AckDeadline is the duration (in seconds) within which a consumer must
 	// acknowledge processing of a message before it is resent to the queue.
-	// Defaults to 10
+	// Defaults to 10.
 	AckDeadline int
 
 	// RetentionDuration is the duration (in seconds) for which messages are
-	// kept in the queue before they are deleted. Defaults to 604800
+	// kept in the queue before they are deleted.
+	// Defaults to 604800.
 	RetentionDuration int
 
 	// ExpirationPolicy is the duration (in seconds) after which the subscription
 	// should automatically be deleted if there is no active subscriber
 	// activity. This value should be longer than RetentionDuration.
-	// Defaults to 0, meaning it never expires
-	// CAUTION: This option only applies to Google Pub Sub Subscriptions
+	// Defaults to 0, meaning it never expires.
+	// CAUTION: This option only applies to Google Pub Sub Subscriptions.
 	ExpirationPolicy int
 }
